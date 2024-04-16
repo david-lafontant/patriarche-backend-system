98bf44f7bc98f13d09c6d0144ceadd62281bdf2b
David Lafontant <lafontant.david@gmail.com>
2024-04-16 09:03:20 -0400

    Scaffold content

---
+++ db/migrate/20240416130255_create_contents.rb
@@ -0,0 +1,12 @@
+class CreateContents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contents do |t|
+      t.string :title
+      t.string :type
+      t.string :file_path
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
