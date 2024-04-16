4e9b0f13d8578212f3b3349a78a675472e2de7a5
David Lafontant <lafontant.david@gmail.com>
2024-04-16 19:19:22 -0400

    Generate publics controller and actions

---
+++ app/controllers/publics_controller.rb
@@ -0,0 +1,13 @@
+class PublicsController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def articles
+  end
+
+  def newz
+  end
+end
---
+++ test/controllers/publics_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class PublicsControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get publics_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get publics_about_url
+    assert_response :success
+  end
+
+  test "should get articles" do
+    get publics_articles_url
+    assert_response :success
+  end
+
+  test "should get newz" do
+    get publics_newz_url
+    assert_response :success
+  end
+end
