d43281180b00e26dbab11a7e470f3655624009de
David Lafontant <lafontant.david@gmail.com>
2024-04-16 19:19:59 -0400

    Setup custom routes for publics actions and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  # get 'publics/home'
+  # get 'publics/about'
+  # get 'publics/articles'
+  # get 'publics/newz'
   devise_for :users
-  root 'pages#home'
+  root 'publics#home'
   resources :contents
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'about', to: 'publics#about'
+  get 'articles', to: 'publics#articles'
+  get 'newz', to: 'publics#newz'
   # Defines the root path route ("/")
   # root "articles#index"
 end
