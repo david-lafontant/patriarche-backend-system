adacd054c3de21640e79aba31e328bfd985f6e8a
David Lafontant <lafontant.david@gmail.com>
2024-04-17 19:01:46 -0400

    Import javascript assets

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,8 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./html5shiv";
+// import "./jquery.prettyPhoto";
+import "./jquery.isotope.min";
+import "./respond.min";
+import "./main";
