5ba82bd1fb63108125e06f3168958a2ee360887f
David Lafontant <lafontant.david@gmail.com>
2024-04-18 06:34:54 -0400

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ # user = User.new(
+#   name: 'Kogi',
+#   email: 'user@domain.com',
+#   password: 'password123456789',
+#   password_confirmation: 'password123456789'
+# )
+# user.skip_confirmation!
+# user.save!
+
+
+#<User id: 1, name: nil, created_at: "2024-04-17 13:55:18.760111000 +0000", updated_at: "2024-04-17 13:55:46.828438000 +0000", email: "naopanda@panda.org">,
+#<User id: 2, name: nil, created_at: "2024-04-17 23:46:21.330056000 +0000", updated_at: "2024-04-17 23:47:44.438188000 +0000", email: "kogi@example.com">,
+ #<User id: 3, name: "Kogi", created_at: "2024-04-18 00:05:24.442438000 +0000", updated_at: "2024-04-18 00:05:24.442438000 +0000", email: "user@domain.com">
+ #
+
+
+#  Article.create!(title: "le grand blond", subtitle: 'Avec un chaussure noire', description: 'Les aventures du grand blond', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id:3)
+#  Article.create!(title: "le retour grand blond", subtitle: 'Chaos a Rio', description: 'I apologize', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 3)
+#  Article.create!(title: "Une histoire du Bronx", subtitle: 'Mi gran amor te di', description: 'You belong to me', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 3)
+#  Article.create!(title: "The Mission", subtitle: 'Emotion', description: 'Enjoy the silence', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 2)
+#  Article.create!(title: "Danse avec les loups", subtitle: 'Seven whole days', description: 'I can get know satisfaction', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 2)
+#  Article.create!(title: "Horizonz lointains", subtitle: 'Ma liberte contre la tiene', description: 'Stanger in moscow', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 2)
+#  Article.create!(title: "The thin red line", subtitle: 'par les plaines et les collines', description: 'amor mio', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 1)
+#  Article.create!(title: "Le pont de la riviere kwai", subtitle: 'Un pont trop loin', description: 'Indiana jones', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 1)
+#  Article.create!(title: "Nos etoiles contraires", subtitle: 'Francisac mae laudes', description: 'Regrets', texte: 'Adipisicing consequat aliquip velit fugiat culpa enim incididunt eu et ullamco. Ex esse adipisicing incididunt veniam. Laborum sunt voluptate ut duis aliqua quis culpa magna nulla sint aliqua dolore commodo. Minim cupidatat voluptate consequat excepteur id incididunt consectetur. Incididunt ullamco et dolor incididunt commodo. Qui culpa amet qui minim id tempor aliqua mollit duis amet.', user_id: 1)
