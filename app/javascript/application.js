8a0c9e690612d8db1774854a212e350cc6b73989
David Lafontant <lafontant.david@gmail.com>
2024-04-18 12:01:34 -0400

    Remove unused scriptfiles import

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,8 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import "./html5shiv";
-// import "./jquery.prettyPhoto";
-import "./jquery.isotope.min";
-import "./respond.min";
-import "./main";
+
