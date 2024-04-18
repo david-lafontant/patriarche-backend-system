dbe2106c41cb76c9bcf7290ac400d8e7a3c48158
David Lafontant <lafontant.david@gmail.com>
2024-04-18 04:06:25 -0400

    Setup new custom routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   # get 'publics/newz'
   devise_for :users
   root 'publics#home'
+  get 'about', to: 'publics#about'
+  get 'reportages', to: 'publics#articles'
+  get 'newz', to: 'publics#newz'
   resources :contents
   resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'about', to: 'publics#about'
-  get 'articles', to: 'publics#articles'
-  get 'newz', to: 'publics#newz'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
