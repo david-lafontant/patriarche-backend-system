357758221caeaae31601e1b39375da3613f41d3c
David Lafontant <lafontant.david@gmail.com>
2024-04-18 06:31:14 -0400

    Add show and reportage actions, remove articles actions

--- app/controllers/publics_controller.rb
+++ app/controllers/publics_controller.rb
@@ -1,13 +1,15 @@
 class PublicsController < ApplicationController
-  def home
+  def home; end
+
+  def about; end
+
+  def show
+    @reportage = Article.find(params[:id])
   end
 
-  def about
+  def reportages
+    @reportages = Article.all
   end
 
-  def articles
-  end
-
-  def newz
-  end
+  def newz; end
 end
