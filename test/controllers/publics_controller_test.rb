39d7610698b14cf3c2342c051add5ada845e44da
David Lafontant <lafontant.david@gmail.com>
2024-04-17 20:17:03 -0400

    Autofix linter errors

--- test/controllers/publics_controller_test.rb
+++ test/controllers/publics_controller_test.rb
@@ -1,22 +1,22 @@
-require "test_helper"
+require 'test_helper'
 
 class PublicsControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do
     get publics_home_url
     assert_response :success
   end
 
-  test "should get about" do
+  test 'should get about' do
     get publics_about_url
     assert_response :success
   end
 
-  test "should get articles" do
+  test 'should get articles' do
     get publics_articles_url
     assert_response :success
   end
 
-  test "should get newz" do
+  test 'should get newz' do
     get publics_newz_url
     assert_response :success
   end
