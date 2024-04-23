337558110bf323becee39880db34055f6edb7209
David Lafontant <lafontant.david@gmail.com>
2024-04-23 17:00:13 -0400

    Add rooutes for reportages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  # get 'publics/home'
-  # get 'publics/about'
-  # get 'publics/articles'
-  # get 'publics/newz'
-  devise_for :users
+
   root 'publics#home'
   get 'about', to: 'publics#about'
   get 'newz', to: 'publics#newz'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  resources :reportages
 end
