53206dd3fed1b21e517cf4192f7f9636b593cd12
David Lafontant <lafontant.david@gmail.com>
2024-04-28 17:22:02 -0400

    Generated new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_23_135140) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_28_202947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,13 +42,35 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_23_135140) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "reportages", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "subltitle"
+    t.text "description"
+    t.text "contenu"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "emission_contents", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.bigint "emission_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["emission_id"], name: "index_emission_contents_on_emission_id"
+    t.index ["user_id"], name: "index_emission_contents_on_user_id"
+  end
+
+  create_table "emissions", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_reportages_on_user_id"
+    t.index ["user_id"], name: "index_emissions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,5 +108,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_23_135140) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "reportages", "users"
+  add_foreign_key "articles", "users"
+  add_foreign_key "emission_contents", "emissions"
+  add_foreign_key "emission_contents", "users"
+  add_foreign_key "emissions", "users"
 end
