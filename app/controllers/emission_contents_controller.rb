d245fd969f86029ed388a7a7e4d0d40228e9074f
David Lafontant <lafontant.david@gmail.com>
2024-04-28 17:14:53 -0400

    Make it madatory to ser to sign in before accessing emission_content and make current user default creator of an emission_content

--- app/controllers/emission_contents_controller.rb
+++ app/controllers/emission_contents_controller.rb
@@ -1,5 +1,6 @@
 class EmissionContentsController < ApplicationController
-  before_action :set_emission_content, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :set_emission_content, only: %i[show edit update destroy]
 
   # GET /emission_contents or /emission_contents.json
   def index
@@ -22,10 +23,13 @@ class EmissionContentsController < ApplicationController
   # POST /emission_contents or /emission_contents.json
   def create
     @emission_content = EmissionContent.new(emission_content_params)
+    @emission_content.user = current_user
 
     respond_to do |format|
       if @emission_content.save
-        format.html { redirect_to emission_content_url(@emission_content), notice: "Emission content was successfully created." }
+        format.html do
+          redirect_to emission_content_url(@emission_content), notice: 'Emission content was successfully created.'
+        end
         format.json { render :show, status: :created, location: @emission_content }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +42,9 @@ class EmissionContentsController < ApplicationController
   def update
     respond_to do |format|
       if @emission_content.update(emission_content_params)
-        format.html { redirect_to emission_content_url(@emission_content), notice: "Emission content was successfully updated." }
+        format.html do
+          redirect_to emission_content_url(@emission_content), notice: 'Emission content was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @emission_content }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +58,20 @@ class EmissionContentsController < ApplicationController
     @emission_content.destroy
 
     respond_to do |format|
-      format.html { redirect_to emission_contents_url, notice: "Emission content was successfully destroyed." }
+      format.html { redirect_to emission_contents_url, notice: 'Emission content was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_emission_content
-      @emission_content = EmissionContent.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def emission_content_params
-      params.require(:emission_content).permit(:title, :description, :content, :user_id, :emission_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_emission_content
+    @emission_content = EmissionContent.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def emission_content_params
+    params.require(:emission_content).permit(:title, :description, :content, :user_id, :emission_id)
+  end
 end
