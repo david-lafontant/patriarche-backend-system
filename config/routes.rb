7f5fa06bbb157df54228a9c9083d9fa9d4e7a6e6
David Lafontant <lafontant.david@gmail.com>
2024-04-28 15:24:06 -0400

    Create routes for articles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'publics#home'
   get 'about', to: 'publics#about'
   get 'newz', to: 'publics#newz'
+  get 'articles', to: 'publics#articles'
+  get '/articles/:id', to: 'publics#show', as: 'article'
   resources :contact_form, only: %i[new create]
   devise_for :users
   resources :reportages
