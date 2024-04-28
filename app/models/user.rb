7b93113eda20e953f4b87f361f07b5982015e7c1
David Lafontant <lafontant.david@gmail.com>
2024-04-28 17:21:15 -0400

    Set User association with article, emission, and emission_content

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable
-  has_many :reportages
+  has_many :articles
+  has_many :emissions
+  has_many :emission_contents
 end
