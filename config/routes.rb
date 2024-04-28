533a2ccb29eff01d90f329b0766332197c803cb2
David Lafontant <lafontant.david@gmail.com>
2024-04-28 17:21:49 -0400

    Setup routes for the new publics' actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :emission_contents
-  resources :emissions
-
   root 'publics#home'
   get 'about', to: 'publics#about'
   get 'newz', to: 'publics#newz'
-  get 'articles', to: 'publics#articles'
-  get '/articles/:id', to: 'publics#show', as: 'article'
-  resources :contact_form, only: %i[new create]
+  get 'published.articles', to: 'publics#publics_articles'
+  get '/published.articles/:id', to: 'publics#show_article'
+  get 'published.emissions', to: 'publics#publics_emissions'
+  get '/published.emissions/:id', to: 'publics#show_emission'
   devise_for :users
+  resources :articles
+  resources :emission_contents
+  resources :emissions
+  resources :contact_form, only: %i[new create]
+  # get 'published.articles', to: 'publics#publics_articles'
+  # get '/published.articles/:id', to: 'publics#show_article', as: 'article'
 end
