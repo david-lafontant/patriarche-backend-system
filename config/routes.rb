362f2c7c68624dd76c9a851d674a2c3813561347
David Lafontant <lafontant.david@gmail.com>
2024-04-29 13:08:55 -0400

    Customize the routes for the publics's articles and emissions

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
 
   root 'publics#home'
   get 'about', to: 'publics#about'
-  get 'newz', to: 'publics#newz'
-  get 'published.articles', to: 'publics#publics_articles'
-  get '/published.articles/:id', to: 'publics#show_article'
-  get 'published.emissions', to: 'publics#publics_emissions'
-  get '/published.emissions/:id', to: 'publics#show_emission'
+  get 'published_articles', to: 'publics#publics_articles'
+  get '/published.articles/:id', to: 'publics#show_article', as: 'un_article'
+  get 'published_emissions', to: 'publics#publics_emissions'
+  get '/published.emissions/:id', to: 'publics#show_emission', as: 'une_emission'
   devise_for :users
   resources :articles
   resources :media
