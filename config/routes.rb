69d7eb51e1c7aef31ed082d789daee2cbf305759
David Lafontant <lafontant.david@gmail.com>
2024-04-29 21:10:45 -0400

    Customize routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root 'publics#home'
   get 'about', to: 'publics#about'
   get 'published_articles', to: 'publics#publics_articles'
-  get '/published.articles/:id', to: 'publics#show_article', as: 'un_article'
+  get 'published_articles/:id', to: 'publics#show_article', as: 'un_article'
   get 'published_emissions', to: 'publics#publics_emissions'
-  get '/published.emissions/:id', to: 'publics#show_emission', as: 'une_emission'
+  get 'published_emissions/:id', to: 'publics#show_emission', as: 'une_emission'
   devise_for :users
   resources :articles
   resources :media
